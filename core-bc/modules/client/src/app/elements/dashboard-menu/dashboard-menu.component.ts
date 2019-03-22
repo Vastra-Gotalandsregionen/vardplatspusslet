fc5cb9d05397ef430662d2c8d30371cd65adca2f
mrg3kko <erik.andersson@monator.com>
2019-03-22 15:21:07 +0100

    Created dashboard menu for dashboard pages

--- core-bc/modules/client/src/app/elements/dashboard-menu/dashboard-menu.component.ts
+++ core-bc/modules/client/src/app/elements/dashboard-menu/dashboard-menu.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 
+import {Router} from "@angular/router";
+
 import {Unit} from "../../domain/unit";
 
 @Component({
@@ -7,34 +9,61 @@ import {Unit} from "../../domain/unit";
   templateUrl: './dashboard-menu.component.html',
   styleUrls: ['./dashboard-menu.component.scss']
 })
+
+
 export class DashboardMenuComponent implements OnInit {
 
   @Input('unit') unit: Unit;
   @Input('type') type: string;
 
-  constructor() {
+  unitDashboardData = new Map<string, string>([
+    ['/', 'Kombinerad'],
+    ['/bed-view', 'Sängar'],
+    ['/kpi-view', 'KPI']
+  ]);
+
+  unitDashboard = [{
+      'path': '',
+      'label': 'Kombinerad'
+    },{
+      'path': '/bed-view',
+      'label': 'Sängar'
+    },{ 
+      'path': '/kpi-view',
+      'label': 'KPI'
+  }];
+
+  dashboardPages : {'label': string, 'route': string, 'isCurrentPage': boolean} [] = [];
+
+  constructor(protected router: Router) {
   }
 
   ngOnInit() {
 
-    console.log('type is: ' + this.type);
-
-    console.log('unit is: ' + this.unit);
-
-    //path: ':managementId/:clinicId/:id/kpi-view',
+    let currentURL = this.router.url;
 
     if(this.type == 'unit-dashboard') {
-      // let urlPrefix = '/' + this.unit.clinic.management.id + '/' + this.unit.clinic.id + '/' + this.unit.id + '/';
+      let urlPrefix = '/' + this.unit.clinic.management.id + '/' + this.unit.clinic.id + '/' + this.unit.id;
 
-      // console.log('urlPrefix: ' + urlPrefix);
+      this.unitDashboard.forEach((page) => {
+
+        let url = urlPrefix + page.path;
+        let isCurrentPage = false;
+
+        if(url == currentURL) {
+          isCurrentPage = true;
+        }
+
+        this.dashboardPages.push({
+          'label': page.label,
+          'route':  url,
+          'isCurrentPage': isCurrentPage
+        });
+
+      });
     }
-
-    //this.unit.
-    //managementId
-    //clinicId
-    //unitId
-
 
   }
 
 }
+
