ba6599d94bbd00a9d317b4ff86352137ae4f64e7
nadei1 <nader.einanlo@vgregion.se>
2019-04-04 08:54:21 +0200

    information om kost kommer som tooltip i kost kolumn. kostlistans PDF får också informationen.

--- core-bc/modules/intsvc/src/main/java/se/vgregion/vardplatspusslet/intsvc/pdf/PdfGenerating.java
+++ core-bc/modules/intsvc/src/main/java/se/vgregion/vardplatspusslet/intsvc/pdf/PdfGenerating.java
@@ -37,48 +37,60 @@ public class PdfGenerating {
         Boolean diet = isTrue(unit.getHasKostFeature());
         Integer antalColumn = motherChildDiet && diet ? 2: diet || motherChildDiet? 1 : 0;
         if (antalColumn == 1 && unit.getHasKostFeature()) {
-            String header[] = {"Säng", "Kost"};
-            table = new PdfPTable(2);
+            String header[] = {"Säng", "Kost", "Info"};
+            table = new PdfPTable(3);
             addTableHeader(table, header);
             for (Bed bed : unit.getBeds()) {
                 String bedName = bed.getLabel();
                 String dietName = isTrue(bed.getOccupied()) ? (bed.getPatient().getDiet() != null? bed.getPatient().getDiet().getName(): "" ): "";
+                String info = dietName.length() > 0 ? bed.getPatient().getInfoDiet(): "";
                 if (dietName.length() > 0)
                 {
                     table.addCell(bedName);
                     table.addCell(dietName);
+                    table.addCell(info);
                 }
             }
         } else if (antalColumn == 1 && unit.getHasMotherChildDietFeature()) {
-            String header[] = {"Säng", "Kost Mor", "Kost barn"};
-            table = new PdfPTable(3);
+            String header[] = {"Säng", "Kost Mor", "Info", "Kost barn",  "Info"};
+            table = new PdfPTable(5);
             addTableHeader(table, header);
             for (Bed bed : unit.getBeds()) {
                 String bedName = bed.getLabel();
                 String dietMother = bed.getOccupied() ? (bed.getPatient().getDietMother() != null ?  bed.getPatient().getDietMother().getName(): "") : "";
                 String dietChild = bed.getOccupied() ? (bed.getPatient().getDietChild() != null ?  bed.getPatient().getDietChild().getName(): "") : "";
+                String infoMother = dietMother.length() > 0 ? bed.getPatient().getInfoDietMother(): "";
+                String infoChild = dietChild.length() > 0 ? bed.getPatient().getInfoDietChild(): "";
                 if (dietMother.length() > 0 || dietChild.length() > 0)
                 {
                     table.addCell(bedName);
                     table.addCell(dietMother);
+                    table.addCell(infoMother);
                     table.addCell(dietChild);
+                    table.addCell(infoChild);
                 }
             }
         }
         else if (antalColumn == 2) {
-            String header[] = {"Säng", "Kost Mor", "Kost barn", "Kost"};
-            table = new PdfPTable(4);
+            String header[] = {"Säng", "Kost Mor", "Info", "Kost barn", "Info", "Kost", "Info"};
+            table = new PdfPTable(7);
             addTableHeader(table, header);
             for (Bed bed : unit.getBeds()) {
                 String bedName = bed.getLabel();
                 String dietMother = Boolean.TRUE.equals(bed.getOccupied()) ? (bed.getPatient().getDietMother() != null ?  bed.getPatient().getDietMother().getName(): "") : "";
                 String dietChild = Boolean.TRUE.equals(bed.getOccupied()) ? (bed.getPatient().getDietChild() != null ?  bed.getPatient().getDietChild().getName(): "") : "";
                 String dietPatient = Boolean.TRUE.equals(bed.getOccupied()) ? (bed.getPatient().getDiet() != null ?  bed.getPatient().getDiet().getName(): "") : "";
+                String infoMother = dietMother.length() > 0 ? bed.getPatient().getInfoDietMother(): "";
+                String infoChild = dietChild.length() > 0 ? bed.getPatient().getInfoDietChild(): "";
+                String info = dietPatient.length() > 0 ? bed.getPatient().getInfoDiet(): "";
                 if (dietMother.length() > 0 || dietChild.length() > 0 || dietPatient.length() > 0) {
                     table.addCell(bedName);
                     table.addCell(dietMother);
+                    table.addCell(infoMother);
                     table.addCell(dietChild);
+                    table.addCell(infoChild);
                     table.addCell(dietPatient);
+                    table.addCell(info);
                 }
             }
         }
