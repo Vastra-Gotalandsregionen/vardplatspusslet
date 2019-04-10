1c8ceb57ce6ad4f214539c2fbe2ab8e0dffa379c
mrg3kko <erik.andersson@monator.com>
2019-04-10 11:53:07 +0200

    Updated styling for statistics. Added icon-link component

--- core-bc/modules/client/src/app/elements/layout/layout.component.ts
+++ core-bc/modules/client/src/app/elements/layout/layout.component.ts
@@ -1,16 +1,14 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent {
 
   constructor() {
   }
 
-  ngOnInit() {
-  }
 
 }
