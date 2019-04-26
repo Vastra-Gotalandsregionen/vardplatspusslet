69944b8486b928a894e8ef4ab06ca10351c6d40c
Patrik Björk <patrik.bjork@codespirit.se>
2019-04-26 09:37:50 +0200

    Add color button to ckeditor (also upgrade ckeditor version). Authorize message requests.

--- core-bc/modules/intsvc/src/main/java/se/vgregion/vardplatspusslet/intsvc/controller/domain/BaseController.java
+++ core-bc/modules/intsvc/src/main/java/se/vgregion/vardplatspusslet/intsvc/controller/domain/BaseController.java
@@ -1,13 +1,32 @@
 package se.vgregion.vardplatspusslet.intsvc.controller.domain;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import se.vgregion.vardplatspusslet.domain.jpa.User;
 import se.vgregion.vardplatspusslet.intsvc.controller.util.HttpUtil;
+import se.vgregion.vardplatspusslet.repository.UserRepository;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 abstract class BaseController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private HttpServletRequest request;
 
     protected String getRequestUserId(HttpServletRequest request) {
         return HttpUtil.getUserIdFromRequest(request).orElse(null);
     }
 
+    protected Optional<User> getUser() {
+        Optional<String> userIdFromRequest = HttpUtil.getUserIdFromRequest(request);
+
+        if (userIdFromRequest.isPresent()) {
+            return Optional.ofNullable(userRepository.findOne(userIdFromRequest.get()));
+        } else {
+            return Optional.empty();
+        }
+    }
 }
